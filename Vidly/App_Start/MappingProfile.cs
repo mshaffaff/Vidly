531ac9f7c81a7f320c4977ab091d5e94230ec9ab
Mehdi <mshaffaf@gmail.com>
2018-01-08 14:03:58 -0500

    RESTFUL - Web Api - Auto Mapper - Postman JSON -

---
+++ Vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Movie, MovieDto>();
+
+
+            // Dto to Domain
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+
+            Mapper.CreateMap<MovieDto, Movie>()
+                      .ForMember(c => c.Id, opt => opt.Ignore());
+        }
+    }
+}
