eaa6de77565b1213ba35e2dc18c1220eb1baffe2
Mehdi <mshaffaf@gmail.com>
2018-01-08 11:33:36 -0500

    Validation-Client Side Validation - Anti-forgeryTokens

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -28,6 +28,7 @@ namespace Vidly.Controllers
             var memebershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = memebershipTypes
 
             };
@@ -50,8 +51,20 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                
+                return View("CustomerForm", viewModel);
+            }
+
             if (customer.Id == 0 )
             {
                 _context.Customers.Add(customer);
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -65,9 +65,9 @@ namespace Vidly.Controllers
             if (movie == null)
                 return HttpNotFound();
 
-            var viewModel = new MovieFormViewModel
+
+            var viewModel = new MovieFormViewModel(movie)
             {
-                Movie = movie,
                 Genres = _context.Genres.ToList()
             };
 
@@ -76,8 +76,23 @@ namespace Vidly.Controllers
 
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new MovieFormViewModel(movie)
+                {
+                    Genres = _context.Genres.ToList()
+
+                };
+
+
+                return View("MovieForm", viewModel);
+
+
+            }
+
             if (movie.Id == 0)
             {
                 movie.DateAdded = DateTime.Now;
